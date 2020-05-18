efc16033ce3c5cc520adc2e88ebeba74d9da5c15
ybnd <ybnd@tuta.io>
2020-05-18 16:12:10 +0200

    Small improvements to dim

--- dim-ddcutil.sh
+++ dim-ddcutil.sh
@@ -7,13 +7,15 @@
 #   - add user to i2c group (usermod -aG i2c $user) and make sure that the i2c group has rw access to /dev/i2c-*
 #   - add udev rule: cp /usr/share/ddcutil/data/45-ddcutil-i2c.rules /etc/udev/rules.d
 
+# todo: add help
+# todo: combine ddcutil / xrandr / wayland into one script
+#			-> common argument parsing
+#			-> configure checks which approach can be used (ddcutil - gnome - xrandr) & 
+# todo: deal with uninitialized ~/.config/dim
+
 
 # Read ~/.dim
-brightness=$(awk -F "=" '/brightness/ {print $2}' ~/.dim)
-prev_brightness=$brightness
-display0=$(awk -F "=" '/display0/ {print $2}' ~/.dim)
-display1=$(awk -F "=" '/display1/ {print $2}' ~/.dim)   # todo: should handle arbitrary nuùber of displays
-                                                        # todo: if not set in ~/.dim, try to query ddcutil, or at least warn the user
+brightness=$(awk -F "=" '/brightness/ {print $2}' ~/.config/dim)
 
 
 if [[ $1 =~ ^[0-9]+$ ]] ;
@@ -22,32 +24,29 @@ then
     brightness=$1
 else
     # Parse commandline arguments & get new brightness value    
-    while getopts ":b:d:e:i:" opt; do
+    while getopts ":configure:" opt; do
         case $opt in
-            b)
-                brightness=$OPTARG
-                ;;
-            d)            
-                brightness=$(echo "$brightness + $OPTARG" | bc) #bash can't parse floating-point!            
-                ;;
-            \?)
-                echo "Invalid option: -$OPTARG" >&2
-                exit 1
-                ;;
-            :)
-                echo "Option $OPTARG requires an argument" >&2
-                exit 1
-                ;;
+        	configure)
+        		# todo: doesn't work...
+        	
+        		# configure displays (this takes too long to do every time the command runs)
+        		# get i2c bus numbers ~ ddcutil & write to ~/.config/dim
+        		let DISPLAY=$(ddcutil detect | sed -rn 's/.*\/dev\/i2c-([[:digit:]])/\1/p' | tr '\n' ',' | sed '$s/,$/\n/')
+				sed -i "s/^display=.*/display=$DISPLAY/" ~/.config/dim
+				;;
         esac
     done
 fi
-let "dbrightness = $brightness"
+let "dbrightness = $brightness"  # todo: what is this and why
 
-# Send DDC in parallel
-# todo: should reformat this to handle more than 2 displays in a readable way while stil running all displays in parallel
-ddcutil -b $display0 setvcp 10 $dbrightness &ddcutil -b $display1 setvcp 10 $dbrightness &&disown  # Look up i2c bus(es) for display!
+declare -a DISPLAY
+IFS=", " read -a DISPLAY <<< "$(awk -F "=" '/display/ {print $2}' ~/.config/dim)"
 
-# Write the new brightness to ~/.dim
-sed -i "s/^brightness=$prev_brightness/brightness=$brightness/" ~/.dim
+for b in "${DISPLAY[@]}"
+do
+	ddcutil -b $b setvcp 10 $dbrightness &  # apply in parallel!
+done
 
+# Write the new brightness to ~/.config/dim
+sed -i "s/^brightness=.*/brightness=$brightness/" ~/.config/dim
 echo "brightness=$brightness"
